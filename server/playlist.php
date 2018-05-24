5c71ddf69273f0150419348916eb7cf14713388d
Unknown <alliluya@yandex.ru>
2018-05-24 14:26:11 +0300

    fixes

--- server/playlist.php
+++ server/playlist.php
@@ -1,20 +1,21 @@
 <?php
   header('Access-Control-Allow-Origin: *');
   include_once("db.func.php");
-  $response = getResponse();
+  $req = $_POST;
+  $response = getResponse($req);
   echo json_encode($response);
     
-  function getResponse() {
-    	if (!isset($_POST['action'])) return [];
-    	switch ($_POST['action']) {
-    		case 'current': return isset($_POST['current_date']) ? pl_last($_POST['current_date'], false) : pl_last("", false); break;
+  function getResponse($req) {
+    	if (!isset($req['action'])) return [];
+    	switch ($req['action']) {
+    		case 'current': return isset($req['current_date']) ? pl_last($req['current_date'], false) : pl_last("", false); break;
     		case 'latest': return pl_last(); break;
-    		case 'get': return isset($_POST['pl_date']) ? pl_get($_POST['pl_date']) : ['error' => 'No playlist date specified']; break;
-    		case 'prev': return isset($_POST['pl_date']) ? pl_next($_POST['pl_date'], true) : ['error' => 'No playlist date specified']; break;
-    		case 'next': return isset($_POST['pl_date']) ? pl_next($_POST['pl_date']) : ['error' => 'No playlist date specified']; break;
-    		case 'top100': return isset($_POST['year']) ? pl_top100($_POST['year']) : ['error' => 'No year specified']; break;
-    		case 'top10artists': return isset($_POST['year']) ? pl_top10artists($_POST['year']) : ['error' => 'No year specified']; break;
-    		case 'delete': return isset($_POST['pl_date']) && isset($_POST['password']) ? pl_delete($_POST['pl_date'], $_POST['password']) :
+    		case 'get': return isset($req['pl_date']) ? pl_get($req['pl_date']) : ['error' => 'No playlist date specified']; break;
+    		case 'prev': return isset($req['pl_date']) ? pl_next($req['pl_date'], true) : ['error' => 'No playlist date specified']; break;
+    		case 'next': return isset($req['pl_date']) ? pl_next($req['pl_date']) : ['error' => 'No playlist date specified']; break;
+    		case 'top100': return isset($req['year']) ? pl_top100($req['year']) : ['error' => 'No year specified']; break;
+    		case 'top10artists': return isset($req['year']) ? pl_top10artists($req['year']) : ['error' => 'No year specified']; break;
+    		case 'delete': return isset($req['pl_date']) && isset($req['password']) ? pl_delete($req['pl_date'], $req['password']) :
     		['error' => 'No playlist date or password specified']; break;
     		default: return [];
     	}
